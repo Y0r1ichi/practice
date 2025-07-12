070b67fe270727f3be3e0d8ce0fecde92a9df4e9
Y0r1ichi <01102006bane@gmail.com>
2025-07-12 23:13:49 +0300

    30.06

---
+++ leetcode28.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int strStr(string haystack, string needle) {
+            int n = haystack.size();
+            int m = needle.size();
+            
+            if (m == 0) return 0;
+            if (n < m) return -1;
+            
+            for (int i = 0; i <= n - m; ++i) {
+                bool found = true;
+                for (int j = 0; j < m; ++j) {
+                    if (haystack[i + j] != needle[j]) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                    return i;
+                }
+            }
+            
+            return -1;
+        }
+    };
